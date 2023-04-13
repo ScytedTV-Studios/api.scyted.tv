b0eeaedf0d7b0d310b6bcdda4c37c456ce2a28f2
CVXSL <70546159+CVXSL@users.noreply.github.com>
2023-04-12 20:33:40 -0600

    Update script.js

--- links/page-code/script.js
+++ links/page-code/script.js
@@ -144,7 +144,7 @@ window.addEventListener('load', () => {
 ////////////////////////////
 
 const nav = document.querySelector('.navbar')
-fetch('https://api.scyted.tv/links/page-code/index.html')
+fetch('/index.html')
 .then(res=>res.text())
 .then(data=>{
     nav.innerHTML=data
