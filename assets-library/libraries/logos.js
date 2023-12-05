b864c2234a9f97cac6b4012b72931eb187bd01b6
Loyd Osborne <loyd.osborne@scyted.tv>
2023-12-05 12:20:02 -0700

    autoCommit 2023 1Tu.122001

---
+++ assets-library/libraries/logos.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://api.scyted.tv/assets-library/libraries/logos.json')
+        .then(response => response.json())
+        .then(data => generateCatalog(data.items))
+        .catch(error => console.error('Error fetching JSON:', error));
+
+    function generateCatalog(items) {
+        const catalogSection = document.getElementById('catalogSection');
+
+        items.forEach(item => {
+            const catalogItem = document.createElement('div');
+            catalogItem.innerHTML = `
+                <div class="preview">
+                    ${generatePreview(item.fileLink, item.fileName)}
+                </div>
+                <div class="file-details">
+                    <h2>${item.fileName}</h2>
+                    <a href="${item.fileDownloadLink}" class="download-btn" download>Download</a>
+                </div>
+            `;
+            catalogSection.appendChild(catalogItem);
+        });
+    }
+
+    function generatePreview(fileLink, fileName) {
+        const fileExtension = getFileExtension(fileName.toLowerCase());
+
+        if (fileExtension === 'jpg' || fileExtension === 'jpeg' || fileExtension === 'png') {
+            return `<img src="${fileLink}" alt="${fileName}">`;
+        } else if (fileExtension === 'mp4' || fileExtension === 'webm') {
+            return `<video controls>
+                        <source src="${fileLink}" type="video/${fileExtension}">
+                        Your browser does not support the video tag.
+                    </video>`;
+        } else if (fileExtension === 'mp3' || fileExtension === 'ogg' || fileExtension === 'wav') {
+            return `<audio controls>
+                        <source src="${fileLink}" type="audio/${fileExtension}">
+                        Your browser does not support the audio tag.
+                    </audio>`;
+        } else {
+            return `<p>Preview not available</p>`;
+        }
+    }
+
+    function getFileExtension(fileName) {
+        return fileName.split('.').pop();
+    }
+});
