e7206acf23f944ddb49a593d2fc7be36fb063a8d
Loyd Osborne <loyd.osborne@scyted.tv>
2024-03-11 00:20:29 -0600

    Update mobile-error.js

--- wave-development/dashboard/mobile-error.js
+++ wave-development/dashboard/mobile-error.js
@@ -24,5 +24,5 @@ document.head.appendChild(style);
 var mobileError = document.createElement('div');
 mobileError.id = 'mobile-error';
 mobileError.class = 'mobile-error';
-mobileError.innerHTML = `<div id="error-message" style="color: red;">ScytedTV Resources isn't currently available to mobile users at this time.</div>`;
+mobileError.innerHTML = `<body><div id="error-message" style="color: red;">ScytedTV Resources isn't currently available to mobile users at this time.</div></body>`;
 document.body.appendChild(mobileError);
